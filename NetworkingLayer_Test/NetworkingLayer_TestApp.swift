817e79496c8f4f1fe6322f2e3a32f7a47294c140
Resendesfr <117100339+Resendesfr@users.noreply.github.com>
2024-04-12 16:23:34 +0100

    Initial Commit

---
+++ NetworkingLayer_Test/NetworkingLayer_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingLayer_TestApp.swift
+//  NetworkingLayer_Test
+//
+//  Created by Filipe Resendes on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingLayer_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkingLayer_TestUITests/NetworkingLayer_TestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkingLayer_TestUITestsLaunchTests.swift
+//  NetworkingLayer_TestUITests
+//
+//  Created by Filipe Resendes on 12/04/2024.
+//
+
+import XCTest
+
+final class NetworkingLayer_TestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
